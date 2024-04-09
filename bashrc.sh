1b4575bd28c0747cb7e93e55705d14d6cbaa893d
Monkadelic-D <riderguy369@hotmail.com>
2024-04-09 17:14:39 -0500

    updated how it all works

---
+++ bashrc.sh
@@ -0,0 +1,17 @@
+# Note: there is no shebang in this script. This script sets my preferred shell
+# configuration and should be able to be sourced from any Bash-like shell or
+# from Z shell.
+
+# If we are not running interactively do not continue loading this file.
+case $- in
+    *i*) ;;
+      *) return;;
+esac
+
+# source any files in our ~/dotfiles/bashrc.d directory
+if [ -x .bashrc.d ]; then
+  for bashrc_file in .bashrc.d/*; do
+    [ -r "$bashrc_file" ] && source "$bashrc_file"
+  done
+  unset bashrc_file
+fi
--- setupdotfiles.sh
+++ setupdotfiles.sh
@@ -3,6 +3,31 @@
 ## Automate configuration of dotfiles, ie:
 ## create .bashrc.d if not present and place bash customizations
 ## there. Also add stanza to .bashrc to look in .bashrc.d if not present
+set -x
+
+## BEGIN VARIABLES
+dtfls_rc_add="[ -r ~/.myshell ] && source ~/.myshell"
+
+# create variables to hold markers for config blocks managed by this script
+dtfls_mng_head="### BEGIN DOTFILES MANAGED BLOCK"
+dtfls_mng_tail="### END DOTFILES MANAGED BLOCK"
+
+# Get OS info to determine package manager
+if [ -e /etc/os-release ]; then
+  source /etc/os-release;
+fi
+if [[ "$ID" == "ubuntu" ]] || [[ "$ID" == "debian" ]] || [[ "$ID" == "linuxmint" ]]; then
+  pkg_mngr="apt"
+elif [[ "$ID" == "fedora" ]] || [[ "$ID" == "rhel" ]]; then
+  pkg_mngr="dnf"
+else
+  echo "Untested OS. Please verify and improve this script."
+  exit 1
+fi
+
+script_dir=$(dirname "$0")
+
+## END VARIABLES
 
 ## BEGIN FUNCTIONS
 
@@ -20,27 +45,14 @@ sudo apt update && sudo apt install -y "$1"
 
 ## BEGIN PREREQUISITES
 
-# get os info
-if [ -e /etc/os-release ]; then
-  source /etc/os-release;
-fi
-if [[ "$ID" == "ubuntu" ]] || [[ "$ID" == "debian" ]] || [[ "$ID" == "linuxmint" ]]; then
-  pkg_mngr="apt"
-elif [[ "$ID" == "fedora" ]] || [[ "$ID" == "rhel" ]]; then
-  pkg_mngr="dnf"
-else
-  echo "Untested OS. Please verify and improve this script."
-  exit 1
-fi
-
-if [[ ! $(which git) ]]; then
+if [[ ! "$(which git)" ]]; then
   if [ "$pkg_mngr" == apt ]; then
     run_apt git
   elif [ "$pkg_mngr" == dnf ]; then
     run_dnf git
   fi
 fi
-if [[ ! $(which vim) ]]; then
+if [[ ! "$(which vim)" ]]; then
   if [ "$pkg_mngr" == apt ]; then
     run_apt vim 
   elif [ "$pkg_mngr" == dnf ]; then
@@ -52,44 +64,36 @@ fi
 
 ## START BASH CUSTOMIZATIONS
 
+# Change to directory containing dotfiles
+if [ "$script_dir" != "$PWD" ]; then
+  cd "$script_dir" || { echo "Failed changing directory to $script_dir"; exit 1; }
+fi
+
 # Create an array of custom bash config files
-bashrc_configs=("$(ls -1A .bash*)")
+bashrc_configs=("$(ls -1 bashrc.*)")
 
 if [ "${#bashrc_configs[@]}" -gt 0 ]; then
   # Create dirctory for custom user bash settings.
   if [ -e "$HOME"/.bashrc ]; then
-    if [ ! -d "$HOME"/.bashrc.d ]; then
-      mkdir "$HOME"/.bashrc.d || { echo "Error creating directory: $HOME/.bashrc.d"; exit 1; }
-    fi
 
   # if the following block isn't present insert it
   # this is taken from the default user .bashrc in Fedora 32
-    if [ grep SETUPDOTFILES.SH "$HOME"/.bashrc ]; then
-      cat << 'EOF' >> "$HOME"/.bashrc
-## BEGIN BLOCK BY SETUPDOTFILES.SH
-if [ -d ~/.bashrc.d ]; then
-    for rc in ~/.bashrc.d/*; do
-        if [ -f "$rc" ]; then
-            . "$rc"
-        fi
-    done
-fi
-unset rc
-## END BLOCK BY SETUPDOTFILES.SH
-EOF
+    grep "$dtfls_mng_head" "$HOME"/.bashrc
+    bashrc_already_modified=$?
+    if [ "$bashrc_already_modified" -gt 0 ]; then
+      echo "$dtfls_mng_head" >> "$HOME"/.bashrc
+      echo "$dtfls_rc_add" >> "$HOME"/.bashrc
+      echo "$dtfls_mng_tail" >> "$HOME"/.bashrc
+    else
+      # change the text between the markers but leave the markers
+      sed -i  "s/($dtfls_mng_head\\n).*(\\n$dtfls_mng_tail)/\$1$dtfls_rc_add\$2/s" "$HOME"/.bashrc
     fi
   fi
-
-  # copy bash files to .bashrc.d directory
-  for rc_file in ${bashrc_configs[@]}; do
-    # if the file already exists, remove it first.
-    if [ -e "$HOME"/"$rc_file" ]; then
-      rm -rf "$HOME"/"$rc_file"
-    fi
-    cp "$rc_file" "$HOME"/.bashrc.d/ ||  { echo "Error copying $rc_file to $HOME/.bashrc.d"; exit 1; }
-  done
 fi
 
+# create or replace a softlink to our bashrc.sh script and config file directory
+ln -sf "$HOME/dotfiles/bashrc.sh" "$HOME/.bashrc.sh"
+ln -sf "$HOME/dotfiles/shell.d" "$HOME/.shell.d"
 ## END BASH CUSTOMIZATIONS
 
 ## START TMUX CUSTOMIZATIONS
@@ -100,7 +104,7 @@ tmux_configs=("$(ls -1d .tmux*)")
 # Find all tmux related stuff
 if [ "${#tmux_file[@]}" -gt 0 ]; then
   # loop through each item found
-  for tmux_file in ${tmux_configs[@]}; do
+  for tmux_file in "${tmux_configs[@]}"; do
     # if it's a directory copy recursively
     if [ -d "$tmux_file" ]; then
       cp -r "$tmux_file" "$HOME"/
@@ -135,3 +139,4 @@ rm -rf ~/.vim/pack/plugins/start/vim-terraform
 git clone https://github.com/hashivim/vim-terraform.git ~/.vim/pack/plugins/start/vim-terraform
 
 ## END VIM CUSTOMIZATIONS
+set +x
