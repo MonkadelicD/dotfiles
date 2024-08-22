46262446ed0be1af818f5d4640e74424efbf1bd2
Monkadelic-D <riderguy369@hotmail.com>
2024-08-21 21:07:55 -0500

    updated os detection to include WSL oddities

--- setupdotfiles.sh
+++ setupdotfiles.sh
@@ -24,19 +24,20 @@ for release_file in $(ls /etc/*release); do
     if [ "${release_file#*"centos"}" != "$release_file" ]; then
       pkg_mngr="yum"
       ID=centos
-    else
-      source "$release_file"
     fi
-    if [[ -z "$ID" ]]; then
-      echo "Untested OS. Please verify and improve this script."
-      exit 1
-    elif [[ "$ID" == "ubuntu" ]] || [[ "$ID" == "debian" ]] || [[ "$ID" == "linuxmint" ]]; then
-      pkg_mngr="apt"
-    elif [[ "$ID" == "fedora" ]] || [[ "$ID" == "rhel" ]] || [[ "$ID" == "rocky" ]]; then
-      pkg_mngr="dnf"
-    fi
+  elif [[ -f "$release_file" ]]; then
+    source "$release_file"
   fi
 done
+
+if [[ -z "$ID" ]]; then
+  echo "Untested OS. Please verify and improve this script."
+  exit 1
+elif [[ "$ID" == "ubuntu" ]] || [[ "$ID" == "debian" ]] || [[ "$ID" == "linuxmint" ]]; then
+  pkg_mngr="apt"
+elif [[ "$ID" == "fedora" ]] || [[ "$ID" == "rhel" ]] || [[ "$ID" == "rocky" ]]; then
+  pkg_mngr="dnf"
+fi
 
 # What is the path to this script?
 script_dir=$(dirname "$0")
